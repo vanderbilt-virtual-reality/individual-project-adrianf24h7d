dcc99c1f3ee066b847a37b81d7f3293382193684
adrianf24h7d <36930328+adrianf24h7d@users.noreply.github.com>
2019-11-18 21:35:08 -0600

    Created Basic 'Is' Block

---
+++ VR_IS_YOU_GAME_1.0/Assets/Scripts/TextToBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextToBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject text = new GameObject();
+        TextMesh t = text.AddComponent<TextMesh>();
+        t.text = "IS";
+        t.fontSize = 30;
+
+        t.transform.localEulerAngles += new Vector3(90, 0, 0);
+        t.transform.localPosition += new Vector3(56f, 3f, 40f);
+
+    }
+}
