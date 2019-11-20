418362ba00fbcf6ae8b6c74a6dc8e23ff79e662e
adrianf24h7d <36930328+adrianf24h7d@users.noreply.github.com>
2019-11-20 00:56:02 -0600

    Created Scripts

---
+++ VR_IS_YOU_GAME_1.0/Assets/Scripts/IsBlockCode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsBlockCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        string adjacent = checkObjectType(other);
+     
+
+    }
+    
+
+    //Checks Collided Object Type
+    public string checkObjectType(Collider other)
+    {
+        string type = other.name;
+        return type;
+    }
+
+    }
