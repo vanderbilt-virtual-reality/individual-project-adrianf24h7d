0598e4e2380fb1b957e699fe997895873aaa82a8
Adrian <adrianf24h7d@gmail.com>
2019-11-20 17:34:55 -0600

    Started Key is Grab Functionality
    Made script folder for generalized 'is' block and made script for changing the layer of the key for grabbing purposes

---
+++ VR_IS_YOU_GAME_1.0/Assets/Scripts/testCode/isBlock.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isBlock : MonoBehaviour
+{
+    public bool touchWin = false;
+    public bool touchGrab = false;
+    public bool touchBox = false;
+    public bool touchFlag = false;
+    public bool touchText = false;
+    public bool touchkey = false;
+
+    public GameObject referenceObject;
+    public ScriptOne referenceScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        referenceObject = GameObject.FindObjectWithTag("key");
+        referenceScript = referenceObject.GetComponent<keyScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(touchkey && touchGrab)
+        {
+            referenceScript.keyLevelToChange = 9;
+        }
+
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "key")
+        {
+            touchkey = true;
+        }
+        if (other.name == "grab")
+        {
+            touchGrab = true;
+        }
+        if (other.name == "win")
+        {
+            touchWin = true;
+        }
+        if (other.name == "box")
+        {
+            touchBox = true;
+        }
+        if(other.name == "flag")
+        {
+            touchFlag = true;
+        }
+        if(other.name == "text")
+        {
+            touchText = true;
+        }
+    }
+ }
