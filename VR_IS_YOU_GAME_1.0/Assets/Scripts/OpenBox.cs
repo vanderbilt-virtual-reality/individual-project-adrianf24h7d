efb036c96aecbd54912102690b2a22424a8a7f9d
adrianf24h7d <36930328+adrianf24h7d@users.noreply.github.com>
2019-11-20 01:58:46 -0600

    Fixed Bugs

---
+++ VR_IS_YOU_GAME_1.0/Assets/Scripts/OpenBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour
+{
+    public Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "key")
+        {
+            anim.enabled = true;
+     
+
+        }
+
+    }
+}
